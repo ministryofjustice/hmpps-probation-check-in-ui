995dc89322a399684e1bff8c5970d28b05f67ea1
Richard Birch <richard.birch@justice.gov.uk>
2026-05-11 13:18:35 +0100

    ESUP-962 Remove unused code

--- server/data/models/uploadLocationResponse.ts
+++
@@ -1,9 +0,0 @@
-import LocationInfo from './locationInfo'
-
-export default class UploadLocationResponse {
-  locationInfo?: LocationInfo
-
-  locations?: LocationInfo[]
-
-  errorMessage?: string
-}
