7a09a139013796842bbf55ff01c1d98498d2a9dd
James King <james.king@tpximpact.com>
2026-09-15 20:40:47 +0100

    Probation account api client

---
+++ server/data/probationAccountApiClient.ts
@@ -0,0 +1,28 @@
+import { RestClient, asSystem } from '@ministryofjustice/hmpps-rest-client'
+import type { AuthenticationClient } from '@ministryofjustice/hmpps-auth-clients'
+import config from '../config'
+import logger from '../../logger'
+
+/**
+ * Client for the Probation Account API
+ */
+export default class ProbationAccountApiClient extends RestClient {
+  constructor(authenticationClient: AuthenticationClient) {
+    super('Probation Account API', config.apis.probationAccountApi, logger, authenticationClient)
+  }
+
+  /**
+   * Registers that the person identified by `crn` wants to hear more about online
+   * probation accounts.
+   */
+  async registerAccountInterest(crn: string): Promise<void> {
+    return this.post<void>(
+      {
+        path: '/probation-accounts/interest',
+        headers: { 'Content-Type': 'application/json' },
+        data: JSON.stringify({ crn }),
+      },
+      asSystem(),
+    )
+  }
+}
--- server/services/index.ts
+++ server/services/index.ts
@@ -1,14 +1,16 @@
 import { dataAccess } from '../data'
 import AuditService from './auditService'
 import EsupervisionService from './esupervisionService'
+import ProbationAccountService from './probationAccountService'
 
 export const services = () => {
-  const { applicationInfo, hmppsAuditClient, esupervisionApiClient } = dataAccess()
+  const { applicationInfo, hmppsAuditClient, esupervisionApiClient, probationAccountApiClient } = dataAccess()
 
   return {
     applicationInfo,
     auditService: new AuditService(hmppsAuditClient),
     esupervisionService: new EsupervisionService(esupervisionApiClient),
+    probationAccountService: new ProbationAccountService(probationAccountApiClient),
   }
 }
 
